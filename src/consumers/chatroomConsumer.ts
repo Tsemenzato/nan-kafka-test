1e5cdbac15272a5826083efeb83c704318f8fb88
tsemenzato <tomas.semenzato@nan-labs.com>
2021-04-09 12:35:32 -0300

    added chatroom read selection

--- src/consumers/chatroomConsumer.ts
+++ src/consumers/chatroomConsumer.ts
@@ -1,29 +1,59 @@
-import { Consumer, EachMessagePayload, Kafka } from "kafkajs";
-import { address } from 'ip';
+import {
+  Consumer,
+  EachMessagePayload,
+  Kafka
+} from "kafkajs";
+import {
+  address
+} from 'ip';
+import ReadLine from 'readline';
 
 const kafka = new Kafka({
   brokers: [`${address()}:9092`],
-  clientId: 'test-producer',
+  clientId: 'test-consumer',
 })
 
-const loggedInUser: string = 'Admin';
-const topic: string = 'chatroom1'
-const consumer: Consumer = kafka.consumer({ groupId: loggedInUser })
+let loggedInUser: string
+
 // topic => chatroom
 // consumerGroup => 1 usuario ??
 // msg => [key: userId,]
-const consumeChatroom = async (topic: string, fromBeginning: boolean = true): Promise<void> => {
+
+const rl = ReadLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const selectChatroom = () => {
+  rl.question(`what chatroom do you want to read? \n`, async (input: string) =>
+    rl.question('input name', async (name)=> {
+      loggedInUser = name
+      await consumeChatroom(input).catch(e => console.error(`[${input}/${loggedInUser}] ${e.message}`, e))})
+    )
+  }
+
+const consumeChatroom = async (topic: string, fromBeginning: boolean = true): Promise < void > => {
+  const consumer: Consumer = kafka.consumer({
+    groupId: loggedInUser
+  })
   await consumer.connect()
-  await consumer.subscribe({ topic, fromBeginning })
+  await consumer.subscribe({
+    topic,
+    fromBeginning
+  })
   console.log(`[Conected to ${topic}]`)
   await consumer.run({
     eachMessage: printMessage,
   })
 }
 
-const printMessage = async ({ topic, partition, message }:EachMessagePayload) => {
+const printMessage = async ({
+  topic,
+  partition,
+  message
+}: EachMessagePayload) => {
   const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
   console.log(`- ${prefix} ~ [From ${message.key}} : ${message.value}`)
 }
 
-consumeChatroom(topic).catch(e => console.error(`[${topic}/${loggedInUser}] ${e.message}`, e))
+selectChatroom()
